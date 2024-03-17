533b5d0d00a93ad7b7cbef33a1de2eb2b4075b87
josepo <jlpolo@gmail.com>
2024-03-17 19:51:51 +0100

    add main cpp file

--- lib/hello.cpp
+++ lib/hello.cpp
@@ -1,5 +1,3 @@
-using namespace std;
-
 #include <string>
 #include "hello.h"
 
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "lib/hello.h";
+
+int main()
+{
+   std::cout << hi() << std::endl;
+
+   return 0;
+}
